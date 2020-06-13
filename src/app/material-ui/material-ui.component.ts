ff656200c9b37d28b4c83317618f84ed32786571
yiern <ernna876@gmail.com>
2020-06-13 12:45:09 +0800

    Last Material Added: Toolbar. Theme: dark

--- src/app/material-ui/material-ui.component.ts
+++ src/app/material-ui/material-ui.component.ts
@@ -6,7 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./material-ui.component.scss']
 })
 export class MaterialUiComponent implements OnInit {
-
+  notification_1:number = 5;
+  notification_2:string = "!";
+  show_spinner:boolean = false
+  
+  loadData(){
+    this.show_spinner = true;
+    setTimeout(() => {
+      this.show_spinner=false;
+    },30000)
+  }
   constructor() { }
 
   ngOnInit(): void {
