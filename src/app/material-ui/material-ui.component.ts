60658aa6834d7c585ba90b36403e0f161c082184
yiern <ernna876@gmail.com>
2020-06-14 20:43:06 +0800

    Finished Layout elements

--- src/app/material-ui/material-ui.component.ts
+++ src/app/material-ui/material-ui.component.ts
@@ -8,13 +8,17 @@ import { Component, OnInit } from '@angular/core';
 export class MaterialUiComponent implements OnInit {
   notification_1:number = 5;
   notification_2:string = "!";
-  show_spinner:boolean = false
-  
+  show_spinner:boolean = false;
+  open_side= false;  
+  Items: string[]=["item 1", "item 2", "item 3"]
   loadData(){
     this.show_spinner = true;
     setTimeout(() => {
       this.show_spinner=false;
-    },30000)
+    },3000)
+  }
+  log(index){
+    console.log(index)
   }
   constructor() { }
 
